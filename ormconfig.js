c53fbded6446cce97808873917d8427506d34d08
LeeonardoVargas <leu1607@hotmail.com>
2020-09-13 09:12:15 -0300

    Removendo comentários dos campos para colocar em produção

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,12 @@
 module.exports = {
   "type": "mongodb",
-  // "url": process.env.MONGO_URL,
-  "host": "localhost", // comentar em produção
-  "database": "discord", // comentar em produção
+  "host": "localhost",
+  "database": "discord",
   "port": 27017,
-  // "logging": true,
   "synchronize": true,
   "useNewUrlParser": true,
   "useUnifiedTopology": true,
   "entities": [
-    // "./dist/infra/schemas/Message.js"
     "./src/infra/schemas/Message.ts"
   ]
 }
