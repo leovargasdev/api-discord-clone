ae57401b25a6cc8a8e7ee6118ba0643161a60f77
LeeonardoVargas <leu1607@hotmail.com>
2020-09-13 09:06:01 -0300

    Adicionando o origin

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ import './providers';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'https://discord.leonardovargas.me' }));
 app.use(routes);
 // Centralizando os erros da aplicação
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
